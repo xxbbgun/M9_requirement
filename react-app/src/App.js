31a420296d2f28c53a38897919ff1b91d9ebda56
xxbbgun <Kanticha_sr@cmu.ac.th>
2021-11-08 12:33:29 +0700

    edit react-router version

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,20 @@
-
+import React from "react";
 import './App.css';
+import {Switch,Route} from "react-router";
+import Signin from "./components/login/Signin";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Switch>
+        <Route path="/">
+          <Signin />
+        </Route>
+        <Route path="/sign-up" >
+          {/* <SignUp /> */}
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ react-app/src/components/login/Signin.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components";
+// import { Link, useHistory } from "react-router-dom";
+function Signin({className }) {
+    return (
+        <div className ={className}>
+        
+            sadfaskjhdalsd
+         
+                sknscspcklz'c
+          
+        </div>
+    )
+}
+
+export default styled(Signin) `
+
+`;
