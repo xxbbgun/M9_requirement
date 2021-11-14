9305b28fc819574f1e9c243f58ceb82a9b14fa38
Bantita Boonyadate <bantita_boonyadate@cmu.ac.th>
2021-11-14 08:02:37 +0700

    edit GetThread file

--- react-app/src/components/Thread/GetThread.js
+++ react-app/src/components/Thread/GetThread.js
@@ -1,41 +1,64 @@
 import React from "react";
 import styled from "styled-components";
-import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 function Comment({ className }) {
   return (
     <div className={className}>
       <div className="container">
-        <Row className="card-thread">
-          <Col className="each-card">
-            <Card.Body className="card-body">
-              <div className="group-thread-title">
-                <div className="thread-username">
-                  <Card.Title>John</Card.Title>
+        <div className="group-thread">
+          <Row className="card-thread">
+            <Col className="each-card">
+              <div className="card-body">
+                <div className="group-thread-title">
+                  <div className="thread-username">
+                    <h4>John</h4>
+                  </div>
+                  <div className="thread-time">
+                    <h4>14/11/2021, 4:42:56 PM</h4>
+                  </div>
                 </div>
-                <div className="thread-time">
-                  <Card.Title>14/11/2021, 4:42:56 PM</Card.Title>
+                <div className="thread-box">
+                  <label>WOW</label>
                 </div>
               </div>
-              
-              <div className="thread-box">
-                <Card.Text>WOW</Card.Text>
+            </Col>
+            <Col className="box-delete">
+              <div className="icon-group">
+                <div className="delete-icon">
+                  <DeleteForeverIcon className="delete" />
+                </div>
               </div>
-            </Card.Body>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </div>
       </div>
     </div>
   );
 }
 
 export default styled(Comment)`
-  .container {
+  .group-thread {
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
+  }
+  .card-thread {
+    margin-top: 30px;
+    transition: 0.5s;
+    width: 80%;
+    border-radius: 15px;
+    box-shadow: 0 2px 8px 0 whitesmoke, 0 6px 10px 0 whitesmoke;
+    border: 1px solid whitesmoke;
+  }
+  .card-thread:hover {
+    box-shadow: 0 4px 8px 0 lightgray, 0 6px 20px 0 lightgray;
   }
   .each-card {
-    margin: 30px;
+    display: flex;
+    justify-content: left;
+  }
+  /* .each-card {
     transition: 0.5s;
     width: 100%;
     border-radius: 15px;
@@ -44,15 +67,27 @@ export default styled(Comment)`
   }
   .each-card:hover {
     box-shadow: 0 4px 8px 0 lightgray, 0 6px 20px 0 lightgray;
-  }
+  } */
   .card-body {
     margin-top: 10px;
     margin-bottom: 10px;
+    width: 600px;
   }
   .group-thread-title {
     display: flex;
   }
   .thread-username {
     margin-right: 90px;
+  }
+  .box-delete {
+    margin-top: 30px;
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: right;
+  }
+  .delete {
+    width: 40px;
+    height: 40px;
+    color: red;
   }
 `;
